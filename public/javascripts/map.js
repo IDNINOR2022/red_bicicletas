b0502d6436db9d1a575da3d84cba4fab276bc7d4
Israel Niño <nrisraeld@correo.udistrital.edu.co>
2022-12-04 16:19:14 -1200

    Subida de proyecto con errores en POST Y DELETE

---
+++ public/javascripts/map.js
@@ -0,0 +1,20 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
+var marker = L.marker([50.3, -0.09]).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+})
